8307f73a3873b9203359a1abc941d855d305864d
hhgttg <ksatoshi119@gmail.com>
2025-10-09 17:20:52 +0900

    start of writing...

---
+++ ball.py
@@ -0,0 +1,18 @@
+import pyxel
+import sprite as sp
+
+####====================================
+#### CONSTANT
+
+####====================================
+#### CLASS
+
+class Ball():
+    def __init__(self):
+        pass
+
+    def __update__(self):
+        pass
+
+    def __draw__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,26 @@
+import pyxel
+import sprite as sp
+
+####====================================
+#### CONSTANT
+
+####====================================
+#### CLASS
+
+class Game:
+    def __init__(self):
+        pyxel.init(256,256),fps=60)
+        pyxel.load("breakout.pyxres")
+
+        pyxel.run(self.update,self.draw)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.bltm(0,0,0,0,0,128,128)
+        
+if __name__=="__main__":
+    Game()
+    
